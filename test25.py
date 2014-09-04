01c81d5b3999acf3c59a15478938c0d3bdd99993
kmcodes <mumbaimenon@gmail.com>
2014-09-04 22:27:06 +0530

    abc

--- test25.py
+++ test25.py
@@ -1,6 +1,21 @@
 import time
 start = time.time()
 ###################################
+def fibonacci():
+    current = 0 
+    previous = 1
+    while True:
+        temp = current
+        current = current + previous
+        previous = temp
+        yield current
+
+
+for index, element in enumerate(fibonacci()):
+    if len(str(element)) >= 1000:
+        answer = index + 1 #starts from 0
+        break
+print(answer)
 
 ###################################
 elapsed = (time.time() - start)
--- try.py
+++ try.py
@@ -1,8 +1,10 @@
 import time
 start = time.time()
 ###################################
-n=1000000
-print len(str(n))
+list_1=[i for i in range(10,-1,-1)]
+
+for index, i in enumerate(list_1):
+    print list_1[index-1]
 ###################################
 elapsed = (time.time() - start)
 print("This code took: " + str(elapsed) + " seconds") 
