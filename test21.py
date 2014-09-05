f359342d4dfee70e5a24a685966e633b611aca5e
kmcodes <mumbaimenon@gmail.com>
2014-09-05 22:12:18 +0530

    21 with old approach

--- test21.py
+++ test21.py
@@ -12,14 +12,55 @@ def d(num):
     for dig in factor_list:
         sum+=dig
     return sum
-
+    
 sum=0
 for i in xrange(1,10000):
     x=d(i)
     y=d(x)
     if i==y and i!=x:
         sum+=i
-print sum        
+print sum      
+    
+
+#create a list containing all the factor sums till the required number    
+# num_list=[]
+# sum_list=[]    
+# for num in range(2,100):
+    # num_list.append(num)
+    # sum_list.append(sum_factors(num))
+# sumhash=dict(zip(sum_list,num_list))
+# numhash=dict(zip(num_list,sum_list))
+# print numhash
+# amicable_numlist=[]
+# for a in numhash:
+    # sum_key=numhash[a]
+    # if sum_key in numhash:
+        # if numhash[sum_key]==a:
+            # amicable_numlist.append(a)
+            # if sum_key in amicable_numlist:
+                # pass
+            # else:    
+                # amicable_numlist.append(sum_key)
+            
+        
+
+# for trial in range(1,10000):    
+    # if trial in sumhash:
+        # if trial=
+        # amicable_num = sumhash.get(trial,"None")
+        # amicable_numlist.append(amicable_num)
+        # if trial in amicable_numlist:
+            # pass
+        # else:    
+            # amicable_numlist.append(trial)
+# print len(amicable_numlist)
+# print amicable_numlist
+
+# summing the result
+# sum=0
+# for i in amicable_numlist:
+    # sum+=i
+# print sum    
 ###################################
 elapsed = (time.time() - start)
 print("This code took: " + str(elapsed) + " seconds") 
