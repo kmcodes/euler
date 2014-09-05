bae8dbdc3b3f4271a8ef24c2679fbba3125bd099
kmcodes <mumbaimenon@gmail.com>
2014-09-05 19:35:30 +0530

    21 added

--- test21.py
+++ test21.py
@@ -1,4 +1,4 @@
-def factors(num):
+def sum_factors(num):
     factor_list=[]
     sum=0
     for i in xrange(1,(int(num**0.5)+1)):
@@ -8,10 +8,23 @@ def factors(num):
                 factor_list.append(num/i)
     for dig in factor_list:
         sum+=dig
-    return factor_list
-def sum_list(factor_list):
-    for dig in factor_list:
-        sum+=dig
-    return sum    
-    
-print sum(factors(220))
+    return sum
+
+#create a list containing all the factor sums till the required number    
+num_list=[]
+sum_list=[]    
+for num in range(1,1000):
+    num_list.append(num)
+    sum_list.append(sum_factors(num))
+sumhash=dict(zip(sum_list,num_list))
+
+for trial in range(1,1000):
+    amicable_numlist=[]
+    if trial in sumhash:
+        amicable_num = sumhash.get(trial,"None")
+        if trial in amicable_numlist:
+            pass
+        else:    
+            amicable_numlist.append(amicable_num)
+            amicable_numlist.append(trial)
+print amicable_numlist            
