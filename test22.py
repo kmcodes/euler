8fd0f87c3ecef433682230fc7ad70efe8c307f02
kmcodes <mumbaimenon@gmail.com>
2014-09-10 16:49:56 +0530

    22 done

--- test22.py
+++ test22.py
@@ -1,53 +1,25 @@
-<<<<<<< HEAD
 import time
 start = time.time()
 ###################################
-def score(x):
-    pass
+def score(name):
+    sum=0
+    for alphabet in name:
+        num=ord(alphabet)-64
+        sum+=num
+    return sum    
+
 f=open("text22.txt",'r')
 names=f.read().split(',')
 f.close
 names.sort()
 
-print ord('A')
+sum=0
 for i,x in enumerate(names,1):
-    pass
-
+    scores=[]
+    x=x[1:-1]
+    sum+=(score(x)*i)
+    
+print sum    
 ###################################
 elapsed = (time.time() - start)
 print("This code took: " + str(elapsed) + " seconds")
-=======
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-#  Project Euler test22.py
-#  
-#  Copyright 2014 kartik <kmcodes@km-pc>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
-
-
-
-def main():
-	
-	return 0
-
-if __name__ == '__main__':
-	main()
-
->>>>>>> 2e56707532fd6c3496a05c27d1333b80f381078c
